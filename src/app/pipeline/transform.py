2505f13c2753681d14bff177b1f0563963c2810a
Frederico Sander <sanderfn@hotmail.com>
2025-07-06 16:04:16 -0300

    Update: atualização transform

--- src/app/pipeline/transform.py
+++ src/app/pipeline/transform.py
@@ -1,13 +1,9 @@
 import pandas as pd
-from Typing import List
-
+from typing import List
 
 """
     função para ler arquivos Excel de uma pasta específica e concatená-los em um único DataFrame.
 """
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
-    
-    if not data_frame_list:
-        raise ValueError("A lista de DataFrames está vazia.")
-    
     return pd.concat(data_frame_list, ignore_index=True)
+
