92641c3f63b1a31c92d9cfe500b2e6302891e015
Frederico Sander <sanderfn@hotmail.com>
2025-07-06 16:02:45 -0300

    Feature: Adicionando o arquivo main

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,9 @@
 from pipeline.extract import extract_from_excel# Importa a função extract_from_excel do módulo analise_vendas_loja_ficticia
+from pipeline.transform import concat_data_frames # Importa a função concat_data_frames do módulo analise_vendas_loja_ficticia
+from pipeline.load import load_excel # Importa a função save_to_excel do módulo analise_vendas_loja_ficticia
 
-lista_de_data_frame = extract_from_excel("data/raw")
-print(lista_de_data_frame) # Chama a função para extrair dados dos arquivos Excel na pasta especificada
+
+if __name__ == "__main__":
+    data_frame_list = extract_from_excel("data/raw") # Chama a função para extrair dados dos arquivos Excel na pasta especificada
+    data_frame = concat_data_frames(data_frame_list) # Concatena os DataFrames extraídos em um único DataFrame
+    load_excel(data_frame, "data/processed", "vendas_consolidadas") # Salva o DataFrame consolidado em um arquivo Excel na pasta especificada
