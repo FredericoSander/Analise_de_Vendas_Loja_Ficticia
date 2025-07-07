e1234cc076f21763da6f9a22d2e1dba75f45c474
Frederico Sander <sanderfn@hotmail.com>
2025-07-07 18:30:28 -0300

    Update: Atualização do main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,21 @@
-from pipeline.extract import extract_from_excel# Importa a função extract_from_excel do módulo analise_vendas_loja_ficticia
-from pipeline.transform import concat_data_frames # Importa a função concat_data_frames do módulo analise_vendas_loja_ficticia
-from pipeline.load import load_excel # Importa a função save_to_excel do módulo analise_vendas_loja_ficticia
+"""Este módulo contém a função principal do pipeline de ETL.
+
+Esse é o arquivo main.py,
+responsável por executar o pipeline de ETL para a análise de vendas.
+"""
+
+from pipeline.extract import extract_from_excel
+# Importa a função extract_from_excel do módulo analise_vendas_loja_ficticia.
+from pipeline.transform import concat_data_frames
+# Importa a função concat_data_frames do módulo analise_vendas_loja_ficticia.
+from pipeline.load import load_excel
+# Importa a função save_to_excel do módulo analise_vendas_loja_ficticia.
 
 
 if __name__ == "__main__":
-    data_frame_list = extract_from_excel("data/raw") # Chama a função para extrair dados dos arquivos Excel na pasta especificada
-    data_frame = concat_data_frames(data_frame_list) # Concatena os DataFrames extraídos em um único DataFrame
-    load_excel(data_frame, "data/processed", "vendas_consolidadas") # Salva o DataFrame consolidado em um arquivo Excel na pasta especificada
+    data_frame_list = extract_from_excel("data/raw")
+    # Chama a função para extrair dados na pasta especificada.
+    data_frame = concat_data_frames(data_frame_list)
+    # Concatena os DataFrames extraídos em um único DataFrame.
+    load_excel(data_frame, "data/processed", "vendas_consolidadas")
+    # Salva o DataFrame consolidado em arquivo Excel na pasta especificada.
