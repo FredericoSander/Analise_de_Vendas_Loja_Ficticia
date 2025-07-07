993349314d97ec11c9d4ad725055b258f04b6e6e
Frederico Sander <sanderfn@hotmail.com>
2025-07-07 18:33:43 -0300

    Update: Atualização do transform.py

--- src/app/pipeline/transform.py
+++ src/app/pipeline/transform.py
@@ -1,9 +1,12 @@
+"""Transformação de dados para o pipeline de ETL."""
+
 import pandas as pd
 from typing import List
 
-"""
-    função para ler arquivos Excel de uma pasta específica e concatená-los em um único DataFrame.
-"""
-def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
-    return pd.concat(data_frame_list, ignore_index=True)
 
+def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """função para ler arquivos Excel de uma pasta específica.
+
+    Após leitura a função concatena os arquivo em um único DataFrame.
+    """
+    return pd.concat(data_frame_list, ignore_index=True)
